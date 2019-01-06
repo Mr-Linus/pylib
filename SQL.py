f73c9b2db39cf13673a749be47dff2d105a0c6fc
Funky <708863861@qq.com>
2019-01-06 20:37:01 +0800

    update functions

--- SQL.py
+++ SQL.py
@@ -2,13 +2,7 @@ import pymysql
 
 
 class SQL:
-    MYSQLSERVER = 'k8s.geekfan.top'
 
-    MYSQLUSER = 'root'
-
-    MYSQLPASSWORD = 'GeekCloud'
-
-    MYSQLDATABASE = 'SYSTEMLIB'
 
     db = pymysql.connect(
         host=MYSQLSERVER,
