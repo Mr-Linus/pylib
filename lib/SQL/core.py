a22fe14a4b9c6ccbc9562634343ce7ef1486ed55
Funky <708863861@qq.com>
2019-01-31 00:06:59 +0800

    add sql func

--- lib/SQL/core.py
+++ lib/SQL/core.py
@@ -1,6 +1,7 @@
 import pymysql  # import MySQLdb   as pymysql
 from settings import MYSQLConfig
 
+
 class SQL:
     MYSQLSERVER = MYSQLConfig['MYSQLSERVER']
     port = MYSQLConfig['port']
@@ -27,14 +28,44 @@ class SQL:
             print(command)
 
     def update_char(self, table, data_type, context):
-        update_sql = "UPDATE "+str(table)+" SET CONTEXT=\'"+str(context)+"\' WHERE TYPE=\'"+str(
+        sql = "UPDATE "+str(table)+" SET CONTEXT=\'"+str(context)+"\' WHERE TYPE=\'"+str(
             data_type)+"\'"
-        self.run(update_sql)
+        self.run(sql)
 
     def update_num(self, table, data_type, context):
-        update_sql = "UPDATE " + str(table) + " SET CONTEXT=" + str(context) + " WHERE TYPE=\'" + str(
+        sql = "UPDATE " + str(table) + " SET CONTEXT=" + str(context) + " WHERE TYPE=\'" + str(
             data_type) + "\'"
-        self.run(update_sql)
+        self.run(sql)
+
+    def create_table(self,table_name, list_name):
+        sql = "CREATE TABLE " + str(table_name) + " ( "+str(list_name) + " ) "
+        self.run(sql)
+
+    def alter_table_rename(self, table_name, new_name):
+        sql = "ALTER TABLE " + str(table_name) + " RENAME " + str(new_name)
+        self.run(sql)
+
+    def alter_table_field_modify(self, table_name, field, type_size):
+        sql = "ALTER TABLE " + str(table_name) + " MODIFY " + str(field) + " " + str(type_size)
+        self.run(sql)
+
+    def alter_table_field_rename(self, table_name, field_old_name, field_new_name, type_size):
+        sql = "ALTER TABLE " + str(table_name) + " CHANGE " + str(field_old_name) + " "\
+              + str(field_new_name) + " " + str(type_size)
+        self.run(sql)
+
+    def delete_table(self, table_name):
+        sql = "DROP TABLE " + str(table_name)
+        self.run(sql)
+
+    def delete_table_field(self, table_name, field_name):
+        sql = "ALTER TABLE " + str(table_name) + " DROP " + str(field_name)
+        self.run()
+
+    # def delete_char(self, table, data_type, context):
+    #     sql = "DELETE "+str(table)+" SET CONTEXT=\'"+str(context)+"\' WHERE TYPE=\'"+str(
+    #         data_type)+"\'"
+    #     self.run(sql)
 
     def close(self):
         self.db.close()
